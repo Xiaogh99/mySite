f34157f42b720cbe96f9501f480f2de759cd1297
华仔 <763593659@qq.com>
2024-06-29 23:34:45 +0800

    create a model, register the app CivilStandard into settings

--- mysite/CivilStandard/models.py
+++ mysite/CivilStandard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Standard(models.Model):
+    title = models.CharField(max_length=100)
+    codeID = models.CharField(max_length=100)
+    published_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title
