63ed447e343855d3bc2108be4ff85ad17ebbd64a
华仔 <763593659@qq.com>
2024-06-30 17:03:11 +0800

    create forms and handle user input

--- mysite/CivilStandard/views.py
+++ mysite/CivilStandard/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ArticleForm
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            return HttpResponse("Article saved!")
+    else:
+        form = ArticleForm
+    return render(request, "home.html", {"form": form})
